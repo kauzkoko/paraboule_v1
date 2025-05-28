f59d38c7c02d85ce9a70e835db1d928e0e612450
kauzkoko <kamelkebab@tutanota.com>
2025-05-28 11:07:20 +0200

    feat: transitions

--- components/Info.vue
+++ components/Info.vue
@@ -1,68 +1,161 @@
 <template>
-    <div class="info-button" v-if="!store.infoScreen" @click="store.infoScreen = true"
-        :class="{ 'info-button-glow': infoButtonGlow }">
-        <h1>i</h1>
-    </div>
-    <div v-else class="info-container"
-        :class="{ 'pointer-events-none': !store.infoScreen }">
-        <div class="info-large">
-            <div class="upper-content">
-                <div>
-                    <h1 v-html="store.infoStepper.current.title"></h1>
-                    <img v-if="store.infoStepper.current.img" :src="store.infoStepper.current.img" />
-                    <p class="mt-10px!" v-html="store.infoStepper.current.subtitle"></p>
-                </div>
-            </div>
-            <div class="lower-content">
-                <div v-if="store.infoStepper.isCurrent('welcome')"
-                    class="mt--10px! flex justify-center flex-col items-center">
-                    <div class="info-next mb-10px" @click="store.infoStepper.goToNext">NEXT</div>
-                    <p>Tap NEXT to learn more on how<br> to use PARABOULE.</p>
-                    <div class="info-close mb-10px mt-20px" @click="close()">CLOSE</div>
-                    <p class="color-hex-ff0000">Tap CLOSE to return to the interface.</p>
 
+    <div class="info-container">
+        <div class="info-large" :class="{ 'info-button-glow': infoButtonGlow, 'info-button': !store.infoScreen }">
+            <div v-if="!store.infoScreen" class="w-full h-full flex justify-center items-center"
+                @click="toggleInfoScreen">
+                <div class="text-48px text-white mt--10px">i</div>
+            </div>
+            <div class="inner-grid" v-else>
+                <div class="upper-content">
+                    <div>
+                        <h1 v-html="store.infoStepper.current.title"></h1>
+                        <img v-if="store.infoStepper.current.img" :src="store.infoStepper.current.img" />
+                        <p class="mt-10px!" v-html="store.infoStepper.current.subtitle"></p>
+                    </div>
                 </div>
-                <div v-if="store.infoStepper.isCurrent('center-circle')" class="mt-0px!">
-                    <DoubleTap />
-                    <p class="mt--5px! mb-10px!">DOUBLE TAP to mute audio.</p>
-                    <Up class="mt--10px!" />
-                    <p class="mt--5px! mb-10px!">SWIPE UP to take on Cochonnet perspective.</p>
-                    <p class="mb-10px!">
-                        <Left class="transform translate-y-5px mr-10px" />
-                        <span>SWIPE LEFT or RIGHT</span>
-                        <Right class="transform translate-y-5px ml-10px" /><br>
-                        to switch pages.
-                    </p>
-                    <p class="mb--10px!">SWIPE DOWN to return to start.</p>
-                    <Down />
-                </div>
-                <div v-if="store.infoStepper.isCurrent('functions')" class="mt--10px!">
-                    <Dot class="mix-blend-difference" />
-                    <p class="mt--5px! mb-10px!">TAP once to activate the function.</p>
-                    <LongPress class="mix-blend-difference" />
-                    <p class="mt--5px! mb-10px!">PRESS and HOLD to hear an explanation of the function. RELEASE to stop.
-                    </p>
+                <div class="lower-content">
+                    <div v-if="store.infoStepper.isCurrent('welcome')"
+                        class="mt--10px! flex justify-center flex-col items-center">
+                        <div class="button-container info-next mb-10px border-black!" @click="store.infoStepper.goToNext">NEXT</div>
+                        <p>Tap NEXT to learn more on how<br> to use PARABOULE.</p>
+                        <div class="button-container info-close mb-10px mt-20px" @click="close()">CLOSE</div>
+                        <p class="color-hex-ff0000">Tap CLOSE to return to the interface.</p>
+
+                    </div>
+                    <div v-if="store.infoStepper.isCurrent('center-circle')" class="mt-0px!">
+                        <DoubleTap />
+                        <p class="mt--5px! mb-10px!">DOUBLE TAP to mute audio.</p>
+                        <Up class="mt--10px!" />
+                        <p class="mt--5px! mb-10px!">SWIPE UP to take on Cochonnet perspective.</p>
+                        <p class="mb-10px!">
+                            <Left class="transform translate-y-5px mr-10px" />
+                            <span>SWIPE LEFT or RIGHT</span>
+                            <Right class="transform translate-y-5px ml-10px" /><br>
+                            to switch pages.
+                        </p>
+                        <p class="mb--10px!">SWIPE DOWN to return to start.</p>
+                        <Down />
+                    </div>
+                    <div v-if="store.infoStepper.isCurrent('functions')" class="mt--10px!">
+                        <Dot class="mix-blend-difference" />
+                        <p class="mt--5px! mb-10px!">TAP once to activate the function.</p>
+                        <LongPress class="mix-blend-difference" />
+                        <p class="mt--5px! mb-10px!">PRESS and HOLD to hear an explanation of the function. RELEASE to
+                            stop.
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div v-if="!store.infoStepper.isCurrent('welcome')"
+        </div>
+        <div v-if="!store.infoStepper.isCurrent('welcome')"
                 class="fixed z-1000 bottom-28px left-50dvw transform translate-x--50% flex justify-center">
-                <div class="info-next" :class="{ 'color-white!': !store.infoStepper.isCurrent('welcome') }"
+                <div class="button-container info-next" :class="{ 'color-white!': !store.infoStepper.isCurrent('welcome') }"
                     @click="store.infoStepper.isLast ? close() : store.infoStepper.goToNext()">{{
                         store.infoStepper.isLast ? 'START' : 'NEXT' }}</div>
             </div>
-        </div>
         <div v-if="store.infoScreen && !store.infoStepper.isCurrent('welcome')"
             class="fixed z-1000 top-28px left-50dvw transform translate-x--50% flex justify-center">
-            <div class="info-close" @click="close()">CLOSE</div>
+            <div class="button-container info-close" @click="close()">CLOSE</div>
         </div>
     </div>
 </template>
 <style scoped>
-.info-button {
-    position: fixed;
-    bottom: -30px;
+.info-large {
+    transition: all 1s cubic-bezier(0.4, 0, 0.2, 1);
+
+    position: absolute;
+    bottom: 50%;
     left: 50%;
-    transform: translateX(-50%);
+
+    transform: translate(-50%, 50%);
+
+    width: 160dvw;
+    height: 160dvw;
+    aspect-ratio: 1/1;
+    border-radius: 50%;
+    background-color: var(--info-background);
+    color: var(--black);
+
+    .inner-grid {
+        width: 100%;
+        height: 100%;
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-template-rows: 1fr 1fr;
+        transition: all 0.8s cubic-bezier(0.4, 0, 0.2, 1);
+    }
+
+    /* flex-direction: column; */
+    font-size: 2rem;
+    font-weight: 600;
+    text-align: center;
+    line-height: 1.2;
+    padding: 2rem;
+    z-index: 1000;
+    box-shadow: 0px 4px 38.4px 22px #616BFF inset,
+    6px 20px 119.6px 52px #FFF inset;
+    border: var(--border-width) solid var(--white);
+
+    p {
+        margin: 0;
+        padding: 0;
+        font-size: 24px;
+        font-weight: 500;
+        max-width: 90dvw;
+    }
+
+    .upper-content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        transition: all 0.8s cubic-bezier(0.4, 0, 0.2, 1);
+
+        div {
+            /* margin-top: 112px; */
+            transition: all 0.6s cubic-bezier(0.4, 0, 0.2, 1);
+
+            h1 {
+                font-size: 36px;
+                font-weight: bolder;
+                /* margin-top: 10px; */
+                text-transform: uppercase;
+                margin-bottom: 10px;
+                mix-blend-mode: normal;
+                transition: all 0.4s cubic-bezier(0.4, 0, 0.2, 1);
+            }
+
+            img {
+                width: 84dvw;
+                margin-top: -20px;
+                min-width: 100px;
+                mix-blend-mode: difference;
+                transition: all 0.4s cubic-bezier(0.4, 0, 0.2, 1);
+            }
+        }
+    }
+
+    .lower-content {
+        max-height: 60dvh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        overflow: hidden;
+        transition: all 0.8s cubic-bezier(0.4, 0, 0.2, 1);
+    }
+}
+
+.info-button {
+    transition: all 1s cubic-bezier(0.4, 0, 0.2, 1);
+
+    position: absolute;
+    left: 50%;
+    top: 100%;
+
+    transform: translate(-50%, -60%);
+
     width: 100px;
     height: 100px;
     display: flex;
@@ -81,15 +174,17 @@
     color: black;
     font-weight: bold;
     font-size: 2rem;
-    transition: color 2s ease, box-shadow 2s ease;
     z-index: 1000;
     mix-blend-mode: exclusion;
+    padding: 0;
+    margin: 0;
+    border-color: black;
 }
 
 .info-button-glow {
     box-shadow: 0px 0px 20px 3px #FFF inset;
     color: white;
-    transition: color 2s ease, box-shadow 2s ease;
+    transition: all 1s cubic-bezier(0.4, 0, 0.2, 1);
 }
 
 .info-container {
@@ -108,21 +203,11 @@
     /* overflow: hidden; */
     z-index: 1000;
     mix-blend-mode: exclusion;
+
+    transition: all 1s cubic-bezier(0.4, 0, 0.2, 1);
 }
 
-.info-next {
-    margin-top: 40px;
-    padding: 6px 6px;
-    border-radius: 10px;
-    border-style: solid;
-    border-width: 3px;
-    font-weight: 500;
-    width: fit-content;
-    /* color: var(--white); */
-}
-
-
-.info-close {
+.button-container {
     padding: 6px 6px;
     border-radius: 10px;
     border-style: solid;
@@ -130,76 +215,21 @@
     font-weight: 500;
     text-transform: uppercase;
     width: fit-content;
-    color: var(--red);
     font-size: 32px;
     font-weight: 500;
+    transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+    /* mix-blend-mode: difference; */
 }
 
-.info-large {
-    width: 160dvw;
-    height: 160dvw;
-    aspect-ratio: 1/1;
-    border-radius: 50%;
-    background-color: var(--info-background);
+.info-next {
+    margin-top: 40px;
+    border-color: var(--white);
     color: var(--black);
+}
 
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 1fr 1fr;
 
-    /* flex-direction: column; */
-    font-size: 2rem;
-    font-weight: 600;
-    text-align: center;
-    line-height: 1.2;
-    padding: 2rem;
-    z-index: 1000;
-    box-shadow: 0px 4px 38.4px 22px #616BFF inset, 6px 20px 119.6px 52px #FFF inset;
-    border: var(--border-width) solid var(--white);
-
-    p {
-        margin: 0;
-        padding: 0;
-        font-size: 24px;
-        font-weight: 500;
-        max-width: 90dvw;
-    }
-
-    .upper-content {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-
-        div {
-            /* margin-top: 112px; */
-
-            h1 {
-                font-size: 36px;
-                font-weight: bolder;
-                /* margin-top: 10px; */
-                text-transform: uppercase;
-                margin-bottom: 10px;
-                mix-blend-mode: normal;
-            }
-
-            img {
-                width: 84dvw;
-                margin-top: -20px;
-                min-width: 100px;
-                mix-blend-mode: difference;
-            }
-        }
-    }
-
-    .lower-content {
-        max-height: 60dvh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        overflow: hidden;
-    }
+.info-close {
+    color: var(--red);
 }
 </style>
 
@@ -210,10 +240,12 @@ const store = useProtoStore();
 const infoButtonGlow = ref(false);
 
 setInterval(() => {
-    infoButtonGlow.value = !infoButtonGlow.value;
-    setTimeout(() => {
+    if (!store.infoScreen) {
         infoButtonGlow.value = !infoButtonGlow.value;
-    }, 2000);
+        setTimeout(() => {
+            infoButtonGlow.value = !infoButtonGlow.value;
+        }, 2000);
+    }
 }, 5000);
 
 let counter = 3;
@@ -225,9 +257,19 @@ setInterval(() => {
     }
 }, 3000);
 
+const toggleInfoScreen = () => {
+    if (store.infoScreen) {
+        close();
+    } else {
+        store.infoScreen = true;
+    }
+}
+
 // store.infoStepper.goTo('functions');
 const close = () => {
+    console.log('close');
     store.infoScreen = false;
     store.infoStepper.goTo('welcome');
+    console.log(store.infoScreen);
 }
 </script>
