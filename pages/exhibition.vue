5df571e4ad4e4300d453d32f6540126c93e1b392
kauzkoko <kamelkebab@tutanota.com>
2025-06-18 23:31:24 +0200

    feat: exhibition url

---
+++ pages/exhibition.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="text-white">
+        <h1>HOW TO UNPIN/RESTART/PIN THE SMARTPHONES</h1>
+        <div class="max-w-screen-lg mx-auto p-1">
+            <div>
+                <h2>Smartphone 1 - THE SHOT</h2>
+                <video src="/videos/s1.mp4" autoplay muted loop controls playsinline></video>
+            </div>
+            <div>
+                <h2>Smartphone 2 - ORIENTATION</h2>
+                <video src="/videos/s2.mp4" autoplay muted loop controls playsinline></video>
+            </div>
+
+            <div>
+                <h2>Smartphone 3 - THE VAR</h2>
+                <video src="/videos/s3.mp4" autoplay muted loop controls playsinline></video>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+html, body {
+    background-color: #000;
+    overflow-y: scroll;
+}
+
+video {
+    width: 100%;
+    height: auto;
+    max-width: 100%;
+    object-fit: contain;
+}
+</style>
