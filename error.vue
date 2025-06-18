3a11930d9b4f6fa5b897b62c0aca775eb9c33ae2
kauzkoko <kamelkebab@tutanota.com>
2025-06-18 23:37:39 +0200

    feat: error page

---
+++ error.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <div class="text-white text-4xl">{{ error.statusCode }}</div>
+    <NuxtLink to="/" class="text-white text-4xl">/</NuxtLink>
+  </div>
+</template>
